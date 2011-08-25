7b00387be51714d4d5325dde16d68bf7cc7bd753
Zach Pendleton <zachpendleton@gmail.com>
2011-08-25 09:54:55 -0600

    Version bump to 0.1.0

--- lib/awesomesauce.rb
+++ lib/awesomesauce.rb
@@ -1,1 +1,4 @@
+$:.unshift(File.expand_path(File.dirname(__FILE__)))
 
+require "awesomesauce/synonyms"
+require "awesomesauce/filter"
--- lib/awesomesauce/filter.rb
+++ lib/awesomesauce/filter.rb
@@ -1,5 +1,24 @@
 module Awesomesauce
   class Filter
+    def initialize(app, synonyms = [])
+      @app = app
+      @synonyms = Awesomesauce::Synonyms | synonyms
+    end
 
+    def call(env)
+      status, headers, response = @app.call(env)
+      body = ""
+      response.each do |part|
+        body += part.gsub(/awesome/, get_synonym)
+      end
+
+      headers["Content-Length"] = body.length
+      [status, headers, body]
+    end
+
+    protected
+    def get_synonym
+      @synonyms[rand(@synonyms.length)]
+    end
   end
 end
